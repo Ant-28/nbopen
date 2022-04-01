1275220ffa932cffbc0fdc41d82b3749814c20ad
Ant <62998445+Ant-28@users.noreply.github.com>
2022-04-01 13:08:28 -0700

    Update osx-make-iconset.sh
    1024x1024 fix? 
    idk how to work with shell scripts

--- osx-make-iconset.sh
+++ osx-make-iconset.sh
@@ -7,7 +7,7 @@ echo "      1024x1024 icon that could be used to for the 'retina' 512x512 icon"
 
 iconsetdir=nbopen.iconset
 mkdir -p ${iconsetdir}
-for size in 16 32 128 256 512; do
+for size in 16 32 128 256 512 1024; do
     s2x=$((size*2))
     cp nbopen/icons/ipynb_icon_${size}x${size}.png ${iconsetdir}/icon_${size}x${size}.png
     cp nbopen/icons/ipynb_icon_${s2x}x${s2x}.png ${iconsetdir}/icon_${size}x${size}@2x.png
